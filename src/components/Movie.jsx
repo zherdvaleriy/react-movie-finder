b2d69d9057182896adaaba4d88270e35097d43eb
Molham Dadm <molham.dadam@gmail.com>
2023-11-05 17:01:46 +0100

    addinf for trailer component

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,28 +1,27 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
+const DEFAULT_PLACEHOLDER_IMAGE =
+  "https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg";
 
-const DEFAULT_PLACEHOLDER_IMAGE = 'https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg'
-
-export default function Movie({movie}) {
-
-   
-     const poster = 
-          movie.Poster === 'N/A' ? DEFAULT_PLACEHOLDER_IMAGE : movie.Poster
+export default function Movie({ movie }) {
+  const poster =
+    movie.Poster === "N/A" ? DEFAULT_PLACEHOLDER_IMAGE : movie.Poster;
 
   return (
-    
     <div className="movie">
-       <Link to={`/movie/${movie.imdbID}`}>
-            <h2>
-                {movie.Title}  
-             </h2>
-    
-            <div>
-                <img src={poster} alt={`The movie titled: ${movie.Title}`} width='270' height={400} />
-            </div>
-            <p>({movie.Year}) </p>
-       
-       </Link>
+      <Link to={`/movie/${movie.imdbID}`}>
+        <h2>{movie.Title}</h2>
+
+        <div>
+          <img
+            src={poster}
+            alt={`The movie titled: ${movie.Title}`}
+            width="270"
+            height={400}
+          />
         </div>
-  )
+        <p>({movie.Year}) </p>
+      </Link>
+    </div>
+  );
 }
---
+++ src/components/Trailer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Trailer(youtubeUrl) {
+  return (
+    <div>
+      <a href="#">Youtube Trailer</a>
+    </div>
+  );
+}
+
+export default Trailer;
