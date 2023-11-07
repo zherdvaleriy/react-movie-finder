de03b5ee317d7fb9d188e4eaad313854a0977a87
Valerii Zherdetskyi <zherdvaleriy@gmail.com>
2023-11-07 13:11:19 +0100

    made improvements

--- src/components/Trailer.jsx
+++ src/components/Trailer.jsx
@@ -1,28 +1,28 @@
 import YouTube from 'react-youtube';
 import { useEffect, useState } from 'react';
-import movieTrailer from 'movie-trailer'
+// import movieTrailer from 'movie-trailer'
 
-function Trailer({ movie = {} }) {
+function Trailer({ movie={} }) {
   const [videoId, setVideoId] = useState(null);
 
-  // useEffect(() => {
-  //   const fetchTrailer = async () => {
-  //     const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(movie.Title)}+trailer&key=AIzaSyC0mkL-HcOesvJRmOhbfRS0nGtrlZMC4oI`);
-  //     const data = await response.json();
-  //     setVideoId(data.items[0]?.id?.videoId || null);
-  //   };
-
-  //   fetchTrailer();
-  // }, [movie.Title]);
-
   useEffect(() => {
-    movieTrailer(movie.Title)
-      .then((url) => {
-        const urlParams = new URLSearchParams(new URL(url).search);
-        setVideoId(urlParams.get("v"));
-      })
-      .catch((error) => console.log(error));
+    const fetchTrailer = async () => {
+      const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(movie.Title)}+trailer&key=AIzaSyC0mkL-HcOesvJRmOhbfRS0nGtrlZMC4oI`);
+      const data = await response.json();
+      setVideoId(data.items[0]?.id?.videoId || null);
+    };
+
+    fetchTrailer();
   }, [movie.Title]);
+
+  // useEffect(() => {
+  //   movieTrailer(movie.Title)
+  //     .then((url) => {
+  //       const urlParams = new URLSearchParams(new URL(url).search);
+  //       setVideoId(urlParams.get("v"));
+  //     })
+  //     .catch((error) => console.log(error));
+  // }, [movie.Title]);
 
   return videoId ? (
     <div className="container-name" >
