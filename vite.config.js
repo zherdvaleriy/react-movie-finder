2249086979b725a7f3067b3adb92a30fdd20877c
Valerii Zherdetskyi <zherdvaleriy@gmail.com>
2023-11-07 15:03:55 +0100

    changes in viteconfig made

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,9 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react', 'react-dom', 'react-youtube','movie-trailer']// Add 'react-youtube' to the external dependencies
-    },
-  },
-});
+})
+
 
